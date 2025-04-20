89c9e512fa40585e7b78d574c94c6cbfe5ce594b
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2025-04-20 16:13:19 +0530

    updated backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-// const url = 'https://dfchecker-backend.onrender.com/api';
-const url = 'http://192.168.137.234:3000/api';
+const url = 'https://dfchecker-backend-ehtc.onrender.com/api';
+// const url = 'http://192.168.137.234:3000/api';
 
 export {url};
